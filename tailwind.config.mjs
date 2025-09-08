88239626ee635bb5bfbe34ed59f24a4a48d5e65c
MauroKolman <mauro.kolman1255@gmail.com>
2025-09-08 19:35:12 -0300

    Integración de secciones sobre mi, skills, contacto y reacondicionamiento de seccion proyectos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,18 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				brand: {
+					yellow: "#fef08a",   // Amarillo principal
+          purple: "#6b21a8",   // Morado medio
+          dark: "#0a0a1f",     // Fondo oscuro
+          darker: "#1a0b2e",   // Segundo tono para gradiente
+          text: "#ffffff",     // Texto principal
+          muted: "#cbd5e1",    // Texto secundario
+				},
+			},
+		},
 	},
 	plugins: [],
-}
+};
